518263820aa082bd45cf6fb31437b5aaa8bcaee8
antiluddite <88464949+antiluddite@users.noreply.github.com>
2023-07-12 16:47:34 -0700

    first commit

---
+++ passwordChecker/app.js
@@ -0,0 +1,27 @@
+// show password toggler
+
+let showPasswordBtn = document.querySelector('.show-password');
+let passwordInp = document.querySelector('.password-input');
+
+showPasswordBtn.addEventListener('click', () => {
+    // toggle icon 
+
+    // font awesome class for eye icon
+    showPasswordBtn.classList.toggle('fa-eye'); 
+
+    // font awesome class for slashed eye icon
+    showPasswordBtn.classList.toggle('fa-eye-slash');
+
+    // ternary operator a shorthand for if and else to change the type of password input
+    passwordInp.type = passwordInp.type === 'password' ? 'text' : 'password';
+})
+
+let passwordChecklist = document.querySelectorAll('.list-item');
+
+let validationRegex = [
+    { regex: /.{8,}/ }, // min 8 letters,
+    { regex: /[0-9]/ },
+    { regex: /[]/ },
+    { regex: /[]/ },
+    { regex: /[]/ },
+]
